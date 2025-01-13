6bd8b577a39fc1e832ebcb0a160de74979dcf08b
santiagodev <sanxp18@gmail.com>
2025-01-12 20:45:43 -0400

    select elements from the header into the nodes.js file, and add toggle functionality in the search button to show an input

--- src/navigation.js
+++ src/navigation.js
@@ -7,10 +7,39 @@ nav.addEventListener("click", (event) => {
         console.log("LOGO");
     } else if(target.closest('.nav-links-container')) {
         console.log("NAV LINKS");
+        navLinksContainer.classList.toggle("change");
         navLinks.classList.toggle("inactive");
         navLinks.classList.toggle("active");
     } else if(target.closest('.search')) {
         console.log("SEARCH");
+        const searchInput = document.createElement("input");
+        const closeButtonContainer = document.createElement("div");
+        const closeButtonBar1 = document.createElement("div");
+        const closeButtonBar2 = document.createElement("div");
+        const closeButtonBar3 = document.createElement("div");
+        headerLogo.classList.toggle("inactive");
+        navLinksContainer.classList.toggle("inactive");
+        searchInput.classList.add("search__input");
+        searchInput.placeholder = "Search for a movie";
+        closeButtonContainer.classList.add("search__close");
+        closeButtonContainer.classList.toggle("change");
+        closeButtonBar1.classList.toggle("bar-1");
+        closeButtonBar1.classList.toggle("menu-icon");
+        closeButtonBar2.classList.toggle("bar-2");
+        closeButtonBar2.classList.toggle("menu-icon");
+        closeButtonBar3.classList.toggle("bar-3");
+        closeButtonBar3.classList.toggle("menu-icon");
+        searchButton.classList.toggle("inactive");
+        closeButtonContainer.append(closeButtonBar1, closeButtonBar2, closeButtonBar3);
+        nav.append(closeButtonContainer, searchInput);
+
+        closeButtonContainer.addEventListener("click", () => {
+            searchInput.remove();
+            closeButtonContainer.remove();
+            headerLogo.classList.toggle("inactive");
+            navLinksContainer.classList.toggle("inactive");
+            searchButton.classList.toggle("inactive");
+        });
     }
 })
 
--- src/nodes.js
+++ src/nodes.js
@@ -4,5 +4,8 @@ const headerLogo = document.querySelector('.logo');
 const navLinksContainer = document.querySelector('.nav-links-container');
 const searchButton = document.querySelector('.search');
 //Header nodes--nav links container
+const bar1 = document.querySelector('.bar-1');
+const bar2 = document.querySelector('.bar-2');
+const bar3 = document.querySelector('.bar-3');
 const menuIcon = document.querySelector('.menu-icon');
 const navLinks = document.querySelector('.nav-links');
